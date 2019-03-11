499d8b2bccd795f4fbff81a84b260d939c71c75f
schartun <schartun@gmail.com>
2019-03-10 20:05:07 -0500

    Delete for Change 2.1 ->2-1 ex, and add 2.2 ex

---
+++ chapter_02/2-2. Simple Messages.py
@@ -0,0 +1,7 @@
+#Store a message in a variable, and print that message.
+#Then change the value of your variable to a new message, and print the new message.
+
+mensaje = "Las papas fritas"
+print(mensaje)
+mensaje = "Las papas fritas con salsa de tomate"
+print(mensaje)
