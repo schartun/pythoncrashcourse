f8905d570f878716df824453fa0ad8c79a8809a8
schartun <schartun@gmail.com>
2019-03-12 16:42:32 -0500

    Try It Yourself add ex chap 2

---
+++ chapter_02/2-8. Number Eight.py
@@ -0,0 +1,10 @@
+#Write addition, subtraction, multiplication, and division
+#operations that each result in the number 8. Be sure to enclose your operations
+#in print statements to see the results. You should create four lines that look
+#like this:
+#print(5 + 3)
+
+print(5 + 3)
+print(10 - 2)
+print(4 * 2)
+print(16 / 2)
---
+++ chapter_02/2-9. Favorite Number.py
@@ -0,0 +1,6 @@
+#Store your favorite number in a variable. Then, using
+#that variable, create a message that reveals your favorite number. Print that
+#message.
+
+favo_number = 5
+print("Mi favorite number is: " + str(favo_number))
