847f75f990855c733eb115b24c23f9a4b32d5056
schartun <schartun@gmail.com>
2019-03-14 16:02:00 -0500

    Try It Yourself ex chap 3

---
+++ chapter_03/3-10. Every Function.py
@@ -0,0 +1,12 @@
+#Think of something you could store in a list. For example,
+#you could make a list of mountains, rivers, countries, cities, languages, or anything
+#else you’d like. Write a program that creates a list containing these items
+#and then uses each function introduced in this chapter at least once.
+
+programing_languages = ["python", "javascript", "java", "ruby", "php"]
+print(sorted(programing_languages))
+programing_languages.sort()
+print(programing_languages)
+programing_languages.reverse()
+print(programing_languages)
+print(len(programing_languages))
---
+++ chapter_03/3-9. Dinner Guests.py
@@ -0,0 +1,7 @@
+#Working with one of the programs from Exercises 3-4
+#through 3-7 (page 46), use len() to print a message indicating the number
+#of people you are inviting to dinner.
+
+guests = ["Papa", "Mama", "Illich", "Johanna"]
+message = "Wellcome to the place, el numero de invitados es: "
+print(message + " " + str(len(guests)))
