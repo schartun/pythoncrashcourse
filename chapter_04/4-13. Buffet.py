25c28addd4cadd2299f39251864d80984fbdd836
schartun <schartun@gmail.com>
2019-08-09 19:13:15 -0500

    Add a new basic food for testing

--- chapter_04/4-13. Buffet.py
+++ chapter_04/4-13. Buffet.py
@@ -1,10 +1,10 @@
-# A buffet-style restaurant offers only five basic foods . 
-# Think of five simple foods, and store them in a tuple . 
+# A buffet-style restaurant offers only five basic foods .
+# Think of five simple foods, and store them in a tuple .
 
-#• Use a for loop to print each food the restaurant offers . 
-# • Try to modify one of the items, and make sure that Python rejects the change . 
+#• Use a for loop to print each food the restaurant offers .
+# • Try to modify one of the items, and make sure that Python rejects the change .
 # • The restaurant changes its menu, replacing two of the items with different foods .
-#  Add a block of code that rewrites the tuple, and then use a for loop to print each of the 
+#  Add a block of code that rewrites the tuple, and then use a for loop to print each of the
 #  items on the revised menu .
 
 basic_food = ("arepas", "empanadas", "cachapas", "tequenos", "golfeados")
@@ -14,7 +14,7 @@ for food in basic_food:
 
 #basic_food[0] = "papas"
 
-basic_food = ("toston", "lumpias", "cachapas", "tequenos", "golfeados")
+basic_food = ("toston", "lumpias", "cachapas", "tequenos", "golfeados","cahapas")
 
 for food in basic_food:
     print(food)
