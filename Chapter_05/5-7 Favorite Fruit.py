c0d430b012255ec9b845674f66d3baec4c70649d
schartun <schartun@gmail.com>
2019-08-12 14:28:11 -0500

    Adding more exe. from chapter 5

---
+++ Chapter_05/5-7 Favorite Fruit.py
@@ -0,0 +1,8 @@
+fav_fruit_list = ['apple', 'banana', 'peach']
+
+if 'apple' in fav_fruit_list:
+    print('You really like apple!')
+if 'banana' in fav_fruit_list:
+    print('You really like bananas!')
+if 'peach' in fav_fruit_list:
+    print('You really like peach!')
---
+++ Chapter_05/5.5 Alien Colors_3.py
@@ -0,0 +1,14 @@
+#alien_color2 = 'green'
+alien_color2 = 'yellow'
+#alien_color2 = 'red'
+#alien_color2 = 'blue'
+
+
+if alien_color2 == 'green':
+    print("You just earned 5 points")
+elif alien_color2 == 'yellow':
+    print("You just earned 10 points")
+elif alien_color2 == 'red':
+    print("You just earned 15 points")
+else:
+    print("You dont have a know color")
